4880f8f306d0e8f672ff09c7acc8a2cfaf853c02
Kuznetsov, Andrii [PS] <andrii.kuznetsov@cengage.com>
2025-09-09 10:42:37 +0300

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
-      <Button>Click me</Button>
     </div>
   )
 }
